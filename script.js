f334f24af8b34d5fff9572d14b1e69d23a1c20bb
Henrique <henrique9811@hotmail.com>
2024-11-01 16:05:29 -0300

    refactoring code

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let score = parseInt(document.querySelector('.score').textContent);
 let number = Math.floor(Math.random() * 21);
 
 function checkNumber(){
+    let wrongNumberMessage = '';
     let guessedNumber = parseInt(document.querySelector('.guess').value)
     if(guessedNumber === number){
         document.querySelector('.number').value = number;
@@ -11,17 +12,15 @@ function checkNumber(){
         document.querySelector('.highscore').textContent = score;
         document.querySelector('.number').textContent = number
         document.body.style.backgroundColor = 'green';
+        return 1;
     }
-    else if(guessedNumber < number){
-        document.querySelector('.message').textContent = 'Too low';
-        score--;
-        document.querySelector('.score').textContent = score
-    }
-    else {
-        document.querySelector('.message').textContent = 'Too high';
-        score--;
-        document.querySelector('.score').textContent = score
-    }
+    else if(guessedNumber < number)
+        wrongNumberMessage = 'Too low';
+    else 
+        wrongNumberMessage = 'Too high';
+    document.querySelector('.message').textContent = wrongNumberMessage;
+    score--;
+    document.querySelector('.score').textContent = score
 }
 
 function playAgain(){
