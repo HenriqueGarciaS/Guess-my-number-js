c7593d4bbe61d3099cf4b558733e90de5b76e7be
Henrique <henrique9811@hotmail.com>
2024-11-01 14:01:07 -0300

    play again function

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function checkNumber(){
         document.querySelector('.message').textContent = 'You guessed correctly';
         document.querySelector('.highscore').textContent = score;
         document.querySelector('.number').textContent = number
+        document.body.style.backgroundColor = 'green';
     }
     else if(guessedNumber < number){
         document.querySelector('.message').textContent = 'Too low';
@@ -21,4 +22,15 @@ function checkNumber(){
         score--;
         document.querySelector('.score').textContent = score
     }
+}
+
+function playAgain(){
+    document.querySelector('.number').value = 0;
+    document.querySelector('.message').textContent = 'start guessing';
+    document.querySelector('.highscore').textContent = 0;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.score').textContent = 100;
+    document.body.style.backgroundColor = "#222";
+    score = parseInt(document.querySelector('.score').textContent);
+    number = Math.floor(Math.random() * 21);
 }
